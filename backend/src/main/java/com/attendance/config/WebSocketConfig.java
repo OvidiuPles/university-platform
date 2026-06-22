3dfbd8f42ca7f51a247477e3e37212ebbdc1b58b
Ples Ovidiu <ovidiu.ples23@gmail.com>
2026-06-22 13:26:11 +0300

    add cross origins for websocket

--- backend/src/main/java/com/attendance/config/WebSocketConfig.java
+++ backend/src/main/java/com/attendance/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.attendance.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -9,6 +10,9 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Value("${app.base-url}")
+    private String baseUrl;
     
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -19,7 +23,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("*")
+                .setAllowedOrigins(baseUrl, "http://localhost:8090", "http://localhost:8080")
                 .withSockJS();
     }
 }
