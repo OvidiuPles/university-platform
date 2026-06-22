5be53cbd72573ef9f3d574da7e62535c989138da
Ples Ovidiu <ovidiu.ples23@gmail.com>
2026-06-22 13:27:01 +0300

    remove unused websocket config

--- backend/src/main/java/com/attendance/config/WebSocketConfig.java
+++ backend/src/main/java/com/attendance/config/WebSocketConfig.java
@@ -13,13 +13,12 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Value("${app.base-url}")
     private String baseUrl;
-    
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic");
-        config.setApplicationDestinationPrefixes("/app");
     }
-    
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
